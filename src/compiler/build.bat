66f3fe081dd8a003f4707af10782d88e92e34b90
Tim K <arcadia.swift@gmail.com>
2013-02-12 18:22:46 +0100

    i

--- src/compiler/build.bat
+++ src/compiler/build.bat
@@ -1,17 +1,11 @@
 @echo off
-REM Copyright (©) 2010-2011 sar3th
-set PATH=%~dp0\..\c++\compiler\bin;%PATH%
-
-REM title MinGW Portable Command Prompt
-REM cd /d "%~dp0"
-
 g++ main.cpp -g ^
 -std=c++0x -static-libgcc -static-libstdc++ ^
 src/Scanner.cpp ^
 src/Object.cpp ^
--I"../c++/include" ^
--L"../c++/lib" ^
--l"boost_filesystem-mgw46-mt-1_52" ^
--l"boost_system-mgw46-mt-1_52" ^
--o test.exe -W
+-I "include" ^
+-L "../c++/lib" ^
+-l "boost_filesystem-mgw46-mt-1_52" ^
+-l "boost_system-mgw46-mt-1_52" ^
+-o "../../bin/compiler.exe" -W
 pause
